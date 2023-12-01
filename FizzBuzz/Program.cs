3ee9a3eb409d2ac98d7459bffbbf59a1b1a7f004
Vanessa Pasalidi <van3pasa@hotmail.com>
2023-12-01 09:01:04 +0000

    add console logging of method for numbers 1 to 100

--- FizzBuzz/Program.cs
+++ FizzBuzz/Program.cs
@@ -1,3 +1,12 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using System.Reflection;
 
-Console.WriteLine("Hello, World!");
+var fizzBuzzService = new FizzBuzz.Divisions.FizzBuzz();
+
+
+foreach (int i in Enumerable.Range(1, 100))
+{
+    MethodInfo method = fizzBuzzService.GetType().GetMethod("GetDivision")!;
+    string output = (string)method.Invoke(fizzBuzzService, new object[] { i })!;
+    Console.WriteLine(output);
+}
